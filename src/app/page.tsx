6ff08a8a3197bd2664d31a098f5ed6b0d4f26e86
niaskn <sitikurnia.nurzannah@gmail.com>
2024-06-19 17:47:24 +0700

    company profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,78 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="flex flex-col items-center w-full">
+      <div className="flex flex-col gap-y-10 w-full">
+        <section className="mt-40 mb-16 grid grid-cols-1 lg:grid lg:grid-cols-2 mx-20 md:grid md:grid-cols-1 sm:grid sm:grid-cols-1">
+          <iframe
+            className="w-full h-full object-contain"
+            src="https://www.youtube.com/embed/1jdD9T-Kj8s?si=rFl2Vuo2UUpZocHy"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          ></iframe>
+          <div className="p-4 flex flex-col flex-wrap justify-center items-center text-center space-x-5">
+            <h1 className="text-7xl text-pink-800 font-bold mx-4 mb-4">
+              Welcome Beautygang!
+            </h1>
+            <p className="text-xl text-justif mx-5 text-brown-600">
+              We provide the best beauty products just for you! We
+              are committed to delivering beauty solutions that are effective,
+              safe, and eco-friendly. Explore our
+              collection and let your natural beauty shine every day.
+            </p>
+          </div>
+        </section>
+        <section className="mb-16 bg-pink-300">
+          <div className="mx-10 p-10 h-full">
+            <h2 className="text-3xl text-pink-800 font-bold mb-4">
+              <a href="/about-us">About Us</a>
+            </h2>
+            <p className="text-lg text-brown-600">
+              We are a leading company in the beauty industry, dedicated to
+              providing high-quality products and services. Our team is
+              committed to excellence and innovation.
+            </p>
+          </div>
+        </section>
+        <section className="mb-16 bg-pink-300">
+          <div className="p-10 mx-10 h-full">
+            <h2 className="text-3xl font-bold text-pink-800 mb-2">
+              <a href="/products">Our Products</a>
+            </h2>
+            <div className="mb-2">
+              <h3 className="text-lg font-bold text-pink-700 mb-1">
+                Radiant Glow Serum
+              </h3>
+              <p className="text-md text-brown-300">
+                Serum wajah dengan vitamin C untuk kulit cerah bersinar.
+              </p>
+              <button className="hover:text-gray-500">
+                <a href="/products">learn more...</a>
+              </button>
+            </div>
+            <div className="mb-2">
+              <h3 className="text-lg font-bold text-pink-700 mb-1 mt-4">
+                UV Protection Day Cream
+              </h3>
+              <p className="text-md text-brown-100">
+                Day cream SPF 30 melindungi kulit dari sinar UV.
+              </p>
+              <button className="hover:text-gray-500">
+                <a href="/products">learn more...</a>
+              </button>
+            </div>
+          </div>
+        </section>
+        <section className="mb-16 bg-pink-300">
+          <div className="p-10 mx-10 h-full">
+            <h2 className="text-3xl font-bold text-pink-800 mb-2">
+              What Our Beautygang say
+            </h2>
+            <blockquote>
+              <p>"This is the best product I have ever used!"</p>
+              <cite>- Satisfied Customer</cite>
+            </blockquote>
+          </div>
+        </section>
       </div>
     </main>
   );
