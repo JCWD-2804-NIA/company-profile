3c33d065818de05bb329256634686b02a7f45d83
niaskn <167733712+niaskn@users.noreply.github.com>
2024-06-20 13:38:01 +0700

    Update ProductList.tsx

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,9 +1,23 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import { Product } from "@/utils/interface/product";
 import { getProducts } from "@/api/product";
 import Image from "next/image";
 
-const ProductList = async () => {
+export const getStaticProps = async () => {
   const products: Product[] = await getProducts();
+  return {
+    props: {
+      products,
+    },
+  };
+};
+
+interface Props {
+  products: Product[];
+}
+
+const ProductList = ({ products }: Props) => {
   console.log("products : ", products);
   return (
     <main className="flex flex-col items-center">
@@ -11,10 +25,9 @@ const ProductList = async () => {
         Products List
       </h1>
       <div className="grid grid-cols-3 gap-10">
-      {products.map((product: Product, index: any) => {
-        return (
-          <div className="flex-wrap rounded overflow-hidden shadow-lg p-2">
-            <ul key={index}>
+        {products.map((product: Product, index: number) => (
+          <div className="flex-wrap rounded overflow-hidden shadow-lg p-2" key={index}>
+            <ul>
               <li className="justify-center flex text-xl font-bold text-pink-900 mt-5">
                 {product.name}
               </li>
@@ -22,7 +35,7 @@ const ProductList = async () => {
                 src={product.image}
                 width={300}
                 height={300}
-                alt={"Picture"}
+                alt="Picture"
                 className="w-52 h-52 mx-auto p-5"
               />
               <li className="w-52 h-52 mx-auto p-5 text-brown-100 text-base">
@@ -33,8 +46,7 @@ const ProductList = async () => {
               </li>
             </ul>
           </div>
-        );
-      })}
+        ))}
       </div>
     </main>
   );
